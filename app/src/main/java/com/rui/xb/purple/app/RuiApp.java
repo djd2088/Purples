381772707ac0e554e3a0db8a21018d687623c12c
杜建东 <djd2088@qq.com>
2018-06-06 21:24:42 +0800

    Initial commit

---
+++ app/src/main/java/com/rui/xb/purple/app/RuiApp.java
@@ -0,0 +1,21 @@
+package com.rui.xb.purple.app;
+
+import android.app.Application;
+
+import com.rui.xb.rui_core.app.Rui;
+import com.rui.xb.rui_core.ui.loader.LoadStyle;
+
+/**
+ * Created by Rui on 2018/4/14.
+ */
+
+public class RuiApp extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Rui.getAppConfigurator(this)
+                .withDefaultLoaderStyle(LoadStyle.BallClipRotateIndicator)
+                .configFinish();
+    }
+}
